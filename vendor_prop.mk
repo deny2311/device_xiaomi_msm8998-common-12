51b1621752886524ed56102e5399101fe79a7d16
Huexxx <huexxx@gmail.com>
2022-01-04 12:09:39 +0100

    msm8998-common: Enable IORap app launch prefetching Commit b73d5516136d1a4de39afd74f564daf70b946889 in build/make (rvc-qpr2) disabled IORap by default due to a "regression" without mentioning what the regression is. Considering how it's been working fine for the past 6 months and has demonstrated improvements to app launch time [1], let's re-enable it here.
    [1] https://medium.com/androiddevelopers/improving-app-startup-with-i-o-prefetching-62fbdb9c9020
    
    Change-Id: I6d72179bd7bf5387b373845da8475230c9682a2d

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -157,6 +157,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.vt_avail_ovr=1 \
     persist.vendor.qti.telephony.vt_cam_interface=1
 
+# IORap app launch prefetching using Perfetto traces and madvise
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.iorapd.enable=true
+
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.stagefright.omx_default_rank.sw-audio=1 \
